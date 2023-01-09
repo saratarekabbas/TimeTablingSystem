5da541cd4a480bb530bd6f3d45e841977121fe3a
saratarekabbas <saraatarekk@icloud.com>
2023-01-10 07:52:04 +0800

    Created migration table for public holiday via php artisan

---
+++ TimetablingSystem/database/migrations/2023_01_09_234548_create_public_holiday_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('public_holiday', function (Blueprint $table) {
+            $table->id();
+            $table->string('public_holiday_title');
+            $table->date('public_holiday_start_date');
+            $table->date('public_holiday_end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('public_holiday');
+    }
+};
