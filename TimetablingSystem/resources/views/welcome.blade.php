a02051faf07f9bf1378fce4d754dd9bae51d564b
saratarekabbas <saraatarekk@icloud.com>
2023-01-10 00:28:55 +0800

    Added main page layout by modifying page html and css

--- TimetablingSystem/resources/views/welcome.blade.php
+++ TimetablingSystem/resources/views/welcome.blade.php
@@ -5,9 +5,45 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
     <title>Timetabling System</title>
 </head>
 <body>
-    <h1>Hello test</h1>
+
+<div class="row">
+    <div class="column side">
+        <a href="#">Overview</a>
+        <a href="#">User Applications</a>
+        <a href="#">Public Holidays</a>
+        <a href="#">Programs</a>
+        <a href="#">Venues</a>
+        <a href="#">Courses</a>
+        <a href="#">Timetable</a>
+    </div>
+
+    <div class="column left">
+        <div class="header">
+            <p>Office Admin / Logout.</p>
+        </div>
+
+        <h2>Main Content</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis
+            velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat,
+            purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan
+            convallis.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis
+            velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat,
+            purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan
+            convallis.</p>
+    </div>
+</div>
+
+{{--all components are added/replaced/shown here--}}
+{{--{{$slot}}--}}
+
+{{--footer--}}
+<div class="footer">Created by SSZ Solutions. © 2023</div>
+
 </body>
 </html>
