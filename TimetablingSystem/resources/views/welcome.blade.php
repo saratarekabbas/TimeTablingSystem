2b5b9bb3b73fc161dff771ae92b518d575da72dc
saratarekabbas <saraatarekk@icloud.com>
2023-01-10 01:27:44 +0800

    Fixes

--- TimetablingSystem/resources/views/welcome.blade.php
+++ TimetablingSystem/resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/app.css">
     <script src="/app.js"></script>
-    <link rel="stylesheet" href="/public/assets/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
     <title>Timetabling System</title>
 </head>
 <body>
@@ -15,19 +15,28 @@
 <div class="row">
     <div class="column side">
         <img src="TtS-Logo.png" alt="Logo">
-        <p>  Timetabling System</p>
+        <p> Timetabling System</p>
         <a href="#"> <i class="fa fa-tachometer" aria-hidden="true"></i> Overview</a>
-        <a href="#">User Applications</a>
-        <a href="#">Public Holidays</a>
-        <a href="#">Programs</a>
-        <a href="#">Venues</a>
-        <a href="#">Courses</a>
-        <a href="#">Timetable</a>
+        <a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>
+            User Applications</a>
+        <a href="#"><i class="fa fa-plane" aria-hidden="true"></i>
+            Public Holidays</a>
+        <a href="#"><i class="fa fa-building" aria-hidden="true"></i>
+            Programs</a>
+        <a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i>
+            Venues</a>
+        <a href="#"><i class="fa fa-server" aria-hidden="true"></i>
+            Courses</a>
+        <a href="#">
+            <i class="fa fa-calendar" aria-hidden="true"></i>
+            Timetable</a>
     </div>
 
     <div class="column right">
         <div class="header">
-            <p>Office Admin / Logout.</p>
+            <p>Office Admin
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+            </p>
         </div>
 
         <h2>Main Content</h2>
