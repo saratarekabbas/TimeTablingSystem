05c225ce71b838d05f058f301eb16b9fc9d27f4e
saratarekabbas <saraatarekk@icloud.com>
2023-01-11 07:35:08 +0800

    Added all routings for venue in web.php

--- TimetablingSystem/routes/web.php
+++ TimetablingSystem/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicHolidayController;
+use App\Http\Controllers\VenueController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,20 +25,61 @@ Route::get('/', function () {
     return view('/office-assistant/user-application/userApplications');
 });
 
+//|--------------------------------------------------------------------------
+//|                               OFFICE ASSISTANT
+//|--------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------//
+// 1. User Application Routings
+//----------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//
+// 2. Public Holiday Routings
+//----------------------------------------------------------------------------//
+
 //displays all list of public holidays
 Route::get('/office-assistant/public-holiday/public-holiday-list', [PublicHolidayController::class, 'index']);
-
 //Add a public holiday
 Route::get('/office-assistant/public-holiday/add-public-holiday', [PublicHolidayController::class, 'addPublicHoliday']);
-
 //Save public holiday (Add)
 Route::post('/office-assistant/public-holiday/save-public-holiday', [PublicHolidayController::class, 'savePublicHoliday']);
-
 //Edit public holiday
 Route::get('/office-assistant/public-holiday/edit-public-holiday/{id}', [PublicHolidayController::class, 'editPublicHoliday']);
-
 //Edit public holiday (Update)
 Route::post('/office-assistant/public-holiday/update-public-holiday', [PublicHolidayController::class, 'updatePublicHoliday']);
-
-//Delete
+//Delete public holiday
 Route::get('/office-assistant/public-holiday/delete-public-holiday/{id}', [PublicHolidayController::class, 'deletePublicHoliday']);
+
+
+//----------------------------------------------------------------------------//
+// 3. Programs Routings
+//----------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//
+// 4. Venue Routings
+//----------------------------------------------------------------------------//
+
+//displays all list of venues
+Route::get('/office-assistant/venue/venue-list', [VenueController::class, 'index']);
+//Add a venues
+Route::get('/office-assistant/venue/add-venue', [VenueController::class, 'addVenue']);
+//Save venues (Add)
+Route::post('/office-assistant/venue/save-venue', [VenueController::class, 'saveVenue']);
+//Edit venues
+Route::get('/office-assistant/venue/edit-venue/{id}', [VenueController::class, 'editVenue']);
+//Edit venues (Update)
+Route::post('/office-assistant/venue/update-venue', [VenueController::class, 'updateVenue']);
+//Delete venue
+Route::get('/office-assistant/venue/delete-venue/{id}', [VenueController::class, 'deleteVenue']);
+
+
+//----------------------------------------------------------------------------//
+// 5. Course Routings
+//----------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//
+// 6. Timetable Routings
+//----------------------------------------------------------------------------//
