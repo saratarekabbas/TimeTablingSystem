7856c8c893826ffedba3c4af6f1186d21b0f0c78
saratarekabbas <saraatarekk@icloud.com>
2023-01-23 23:36:44 +0800

    Registration page (No logic yet / routings need to be fixed too)

--- TimetablingSystem/resources/views/registration.blade.php
+++ TimetablingSystem/resources/views/registration.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/app.css">
-    <title>Login Page</title>
+    <title>Registration Page</title>
 </head>
 <body>
 
@@ -15,20 +15,18 @@
         <div class="login-header">
             <img class="login-img" src="TtS-Logo-White.png">
             <h1>Timetabling System</h1>
-            <h2>Login to access your profile</h2>
+            <h2>Register an account now.</h2>
         </div>
 
         <div class="login-form">
             <form>
-                <input type="text" class="login-inputbox" placeholder="Email Address" name="email-address">
-
+                <input type="text" class="login-inputbox" placeholder="Full Name" name="email-address">
+                <input type="email" class="login-inputbox" placeholder="Email Address" name="password">
                 <input type="password" class="login-inputbox" placeholder="Password" name="password">
 
-                <a href="#" class="login-forgotpassword forgotpasswordA">Forgot Password?</a>
+                <input class="login-submit" value="REGISTER">
 
-                <input class="login-submit" value="LOGIN">
-
-                <a href="#" class="login-forgotpassword forgotpasswordB">No account yet? <b>Register here.</b></a>
+                <a href="#" class="login-forgotpassword forgotpasswordB">Already have an account? <b>Login here.</b></a>
             </form>
         </div>
     </div>
